c08d19b611ef3a38851c7ed4e35b05c14f05fb60
Srijan2506 <155107972+Srijan2506@users.noreply.github.com>
2024-08-10 14:53:33 +0530

    Update MovieRepo.java

--- java/com/example/Springboot/watchlist/repository/MovieRepo.java
+++ java/com/example/Springboot/watchlist/repository/MovieRepo.java
@@ -1,4 +1,4 @@
-package com.example.Springboot.watchlist.repository;
+packagecom.example.srijanshukla.Springboot.watchlist.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
