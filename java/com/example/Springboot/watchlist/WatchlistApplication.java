2bc3a990c44efd3298e8df68e319c65f2e4b4dd7
Srijan2506 <155107972+Srijan2506@users.noreply.github.com>
2024-08-10 14:55:42 +0530

    Update WatchlistApplication.java

--- java/com/example/Springboot/watchlist/WatchlistApplication.java
+++ java/com/example/Springboot/watchlist/WatchlistApplication.java
@@ -1,4 +1,4 @@
-package com.example.Springboot.watchlist;
+packagecom.example.srijanshukla.Springboot.watchlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
